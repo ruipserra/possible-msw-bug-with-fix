8628c5fe99d9bd71bfcb18062ee7f652f761e104
Rui Serra <ruipserra@gmail.com>
2023-11-28 19:14:12 -0800

    Allow msw to setup mocks before rendering app

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-if (process.env.NODE_ENV === 'development') {
-  import('./mocks/browser');
+function renderApp() {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+if (process.env.NODE_ENV === 'development') {
+  import('./mocks/browser').then(renderApp);
+} else {
+  renderApp();
+}
